646984f3a1214c183d7f1370f4b4b5ff6aaabd65
LeeSoftRu <s.leesoft@gmail.com>
2013-10-10 01:27:09 +0300

    Displaying of page with table

--- notabenoid/__init__.py
+++ notabenoid/__init__.py
@@ -3,17 +3,21 @@ import urllib, codecs, re
 
 app = Flask('bilingua')
 
-@app.route('/')
-def index():
-    return "Hello"
-
 def extractBetween(text, start, end):
     return re.findall('(' + re.escape(start) + '.*?' + re.escape(end) + ')'
         , text, re.I | re.S)
 
-text = codecs.open(r'M:\cache_hc\notabenoid.com\book\24992\83961', 'r', 'utf8').read()
-text = extractBetween(text, '<table id="Tr"', '</table>')
-text = extractBetween(text[0], '<tbody', '</tbody>')
-cells = extractBetween(text[0], '<td', '</td>')
-for idx, cell in enumerate(cells):
-    print(idx+1, cell)
+@app.route('/')
+def index():
+    text = codecs.open(r'M:\cache_hc\notabenoid.com\book\24992\83961', 'r', 'utf8').read()
+    text = extractBetween(text, '<table id="Tr"', '</table>')
+    text = extractBetween(text[0], '<tbody', '</tbody>')
+    cells = extractBetween(text[0], '<td', '</td>')
+
+    page = '<table border=1>'
+    for idx, cell in enumerate(cells):
+        #print(idx+1, call)
+        page += '<tr>' + cell + '</tr>'
+    page += '</table>'
+
+    return page

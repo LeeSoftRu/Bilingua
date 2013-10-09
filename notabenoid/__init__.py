ea25dfb5b6ad545b2095f3f461a34a8c6f5b53d2
LeeSoftRu <s.leesoft@gmail.com>
2013-10-10 02:34:38 +0300

    Less logic in action - it moved out to functions

--- notabenoid/__init__.py
+++ notabenoid/__init__.py
@@ -7,20 +7,8 @@ def extractBetween(text, start, end):
     return re.findall('(' + re.escape(start) + '.*?' + re.escape(end) + ')'
         , text, re.I | re.S)
 
-@app.route('/')
-def index():
-    return redirect('/24992/83961/1/1')
-
-@app.route('/<bookId>/<chapterId>/<pageId>/<lineNo>')
-def read(bookId, chapterId, pageId, lineNo):
-    filePath=r'M:\cache_hc\notabenoid.com\book\{}\{}^\Orig_page={}' \
-        .format(bookId, chapterId, pageId)
-    text = codecs.open(filePath, 'r', 'utf8').read()
-    text = extractBetween(text, '<table id="Tr"', '</table>')
-    text = extractBetween(text[0], '<tbody', '</tbody>')
-    cells = extractBetween(text[0], '<td', '</td>')
-
-    page = '<table border=1>'
+def processCells(cells):
+    result = []
     for idx, cell in enumerate(cells):
         cell = re.sub('\d+\.\d+\.\d+ в \d+:\d+</p>', '', cell)
         cell = cell.replace('</p>', '[/p]')
@@ -28,13 +16,28 @@ def read(bookId, chapterId, pageId, lineNo):
         cell = re.sub('<.*?>', '', cell)
         cell = cell.replace('[/p]', '</p>')
         cell = re.sub('(</p>\s*)*$', '', cell, re.S)
+        if cell:
+            #print(cell)
+            result.append(cell)
+    return result
 
-        if not cell:
-            continue
+def extractCellsFromFile(bookId, chapterId, pageId):
+    filePath=r'M:\cache_hc\notabenoid.com\book\{}\{}^\Orig_page={}' \
+        .format(bookId, chapterId, pageId)
+    text = codecs.open(filePath, 'r', 'utf8').read()
+    text = extractBetween(text, '<table id="Tr"', '</table>')
+    text = extractBetween(text[0], '<tbody', '</tbody>')
+    cells = extractBetween(text[0], '<td', '</td>')
+    return processCells(cells)
 
-        print(cell)
+@app.route('/')
+def index():
+    return redirect('/24992/83961/1/1/0')
 
+@app.route('/<bookId>/<chapterId>/<pageId>/<lineNo>/<column>')
+def read(bookId, chapterId, pageId, lineNo, column):
+    page = '<table border=1>'
+    for cell in extractCellsFromFile(bookId, chapterId, pageId):
         page += '<tr><td>' + cell + '</td></tr>'
     page += '</table>'
-
     return page

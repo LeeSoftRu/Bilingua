b63c2bbb2dd1445d80cec4afa401a77eb195133c
LeeSoftRu <s.leesoft@gmail.com>
2013-10-10 01:11:07 +0300

    Extract table from page

--- notabenoid/__init__.py
+++ notabenoid/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, redirect, request
+import urllib, codecs, re
 
 app = Flask('bilingua')
 
 @app.route('/')
 def index():
-    return "Hello, World"
+    return "Hello"
+
+page = codecs.open(r'M:\cache_hc\notabenoid.com\book\24992\83961', 'r', 'utf8').read()
+table = re.findall(r'(<table id="Tr".*?</table>)', page, re.I | re.S)
+print(table)
+
+
